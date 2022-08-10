ba4c1a8eb44a97af045aec079d5bd2b40011d668
sanghpark <sanghpark@iotree.co.kr>
2022-08-11 00:57:45 +0900

    first commit

---
+++ src/main/java/kr/co/iotree/sanghProject/SanghProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.co.iotree.sanghProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanghProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanghProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/iotree/sanghProject/SanghProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.iotree.sanghProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanghProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
